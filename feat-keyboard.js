d13b39f46b4850e907a8b29aabaf0129c04a54de
Riser97 <119424167+Riser97@users.noreply.github.com>
2023-05-29 23:56:28 +0800

    feat-keyboard.js

--- feat-keyboard.js
+++ feat-keyboard.js
@@ -26,35 +26,42 @@ const operators = ["AC", "/", "x", "+", "-", "="];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Display = ({ input, output }) => (
-    <div className="output">
-      <span className="result">{output}</span>
-      <span id="display" className="input">{input}</span>
+  <div className="output">
+    <span className="result">{output}</span>
+    <span id="display" className="input">{input}</span>
+  </div>
+);
+
+const Key = ({ keyData: { id, value }, handleInput }) => (
+  <button id={id} onClick={() => handleInput(value)}>
+    {value}
+  </button>
+);
+
+const Keyboard = ({ handleInput }) => (
+  <div className="keys">
+    {calcData.map((key) => (
+      <Key key={key.id} keyData={key} handleInput={handleInput} />
+    ))}
+  </div>
+);
+
+const App = () => {
+const [input, setInput] = React.useState("0");
+  const [output, setOutput] = React.useState("");
+
+
+  const handleInput = () => {}
+
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <Display input={input} output={output} />
+        <Keyboard handleInput={handleInput} />
+      </div>
     </div>
   );
-
-  const Key = ({ keyData: { id, value }, handleInput }) => (
-    <button id={id} onClick={() => handleInput(value)}>
-      {value}
-    </button>
-  );
-  
-  const Keyboard = ({ handleInput }) => (
-    <div className="keys">
-      {calcData.map((key) => (
-        <Key key={key.id} keyData={key} handleInput={handleInput} />
-      ))}
-    </div>
-  );
-
-  const App = () => {
-    const [input, setInput] = React.useState("0");
-      const [output, setOutput] = React.useState("");  
-
-      return (
-        <div>
-            <Display input={input} output={output} />
-        </div>
-      )
-  }
+}
 
 ReactDOM.render(<App />, document.getElementById("app"))
